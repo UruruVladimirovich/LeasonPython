0511cd94d4102b261f75d40d84a346e4d94ade2e
Ururu Vladimirovich <trubnikov-anton@mail.ru>
2025-05-15 17:44:38 +0300

    Python

---
+++ Leason 1 от 12.05.py
@@ -0,0 +1,100 @@
+# name = '1'
+# name1 = 1
+# name2 = 1.2
+# name3 = True;
+
+# userName = 'userName'
+# # Underscorte notation
+# user_name = '1'
+
+# Name = 1
+# name = 2
+# print (name)
+# print('ABC')
+
+
+# Типы данных
+# bool - логические значения
+# int, float, complex, str
+
+# int = 1  # 1,50,30 - целые числа
+# float = 1.2 # Числа с плавающей точкой, в качестве разделителя используется точка.
+# str = "Любые как в JS"
+# # Управляющие последовательностью в строках
+# # \n -  перемещает курсор на новую строку, разбивая текст на части.(осуществляет переход на новую строку)
+# # \t - добавляет табуляцию (4 отступа)
+# text = "Hello\n Python\t Ещё что нибудь"   #Hello
+# print(text)                                # Python  Ещё что нибудь
+
+
+# userName = 'User1'
+# print(f'Пользователь {userName}')     # Пользователь User1
+# print(type(userName))                 # <class 'str'>
+
+# request = int(input('Введите число 1: '))
+# request1 = int(input('Введите число 2: '))
+# math = request + request1
+# print(math)
+
+
+# number = 4.3
+# print(round(number))   # 4
+
+
+# Операции сравнения
+# == - возращает True если оба операнда равны. Иначе возращает False
+# != - возращает True, если оба операнда  НЕ равны, иначе  False
+# 
+ 
+# a = 5
+# b = 6
+# result = a == b
+# print(result)   # False
+
+
+# # Логические операции
+# age = 22
+# weight = 300
+# result = age > 21 and weight == 300     # and === ||
+# result = age > 21 or weight == 300      # or === &&
+
+
+# language = 'english'
+# if language == "english":
+#     print('Английский не руский')    # Английский не руский
+# elif language == 'english'
+#     print("Работает второе условие")
+
+
+# time = int(input('Введите время'))
+# if time > 22 and time < 6:
+#     print("Сейчас ночь, иди спать")
+# elif time > 6 or time < 12:
+#     print('Утро!')
+# else:
+#     print('Это блок элс!')        
+
+
+# Цыклы
+# number = 1 
+# while number < 5:
+#     print(f"number = {number}")
+#     number += 1
+# print('Работа программы завершена')  
+
+
+# for n in range(10):                 # 10 повторений
+#     print("Hello World")
+
+# for n in range(0, 500, 10):  # будет считать с 0 до 500 с шагом 10
+#     print(n)
+
+# for n in range(0, 500):  # будет считать с 0 до 500
+#     print(n)
+
+
+
+# Функции
+# def nameFunction():
+#     print('Пайтон моя жизнь')    # Пайтон моя жизнь
+# nameFunction()
---
+++ Leason 2 от 14.05..py
@@ -0,0 +1,212 @@
+""" Оператор return и возвращение результата из функции
+ """
+# def print_abountme(name):
+#     print("Меня зовут:", name)
+# print_abountme('Ururu')     #  Меня зовут: Ururu
+
+# def print_abountme(name, age = 18):
+#     print("Меня зовут:", name, age)
+# print_abountme('Ururu')     #  Меня зовут: Ururu 18
+
+# def print_abountme(name, age = 18):
+#     print("Меня зовут:", name, age)
+# print_abountme(age =16, name = "Ururu")    #  Меня зовут: Ururu 16
+
+# def print_abountme(name, age,*, city, city1):    #   * означает что можно только так передать все последующие параметры аргумента city = 'Sochi'
+#     print("Меня зовут:", name, age, city)
+# print_abountme("Ururu", "Age", city = 'Sochi', city1 = 'Sochi1')    #  Ururu Age Sochi Sochi1
+
+
+
+# def sum(*numbers):
+#     result = 0
+#     for j in numbers:
+#         result += j
+#     print(f"sum = {result}")    
+
+# def get_info():
+#     return "My Info"
+# print(get_info())
+
+
+# def speed_limited(speed):
+#     if speed > 150:
+#         print("Вы нарушаете, пока")
+#         return
+#     else:
+#         print("Всё ок")
+# speed_limited(200)         #  Вы нарушаете, пока
+
+
+# def say_hello(): print("Hello")
+# def say_goodbye(): print("Goodbye")
+
+# message = say_hello
+# message()                #  Hello
+
+
+# def math_operation(a, b, operation):
+#     result = operation(a, b)
+#     print(f"result = {result}")
+
+# def sum(a,b): return a + b
+# def multiply(a, b): return a * b
+
+# math_operation(555, 111, sum)    #  666
+# math_operation(12,2, multiply)   #  24
+
+
+""" def sum (a, b): return a + b
+def subtract(a, b): return a - b
+def multiply(a, b): return a * b
+
+def select(choice):
+    if choice == 1:
+        return sum
+    elif choice == 2:
+        return subtract
+    else:
+        return multiply
+    
+oper1 = select(1)
+print(oper1(1,2))    #  3
+
+oper2 = select(2)
+print(oper2(4,4))    #  0
+
+oper3 = select(3)
+print(oper3(2,2))    #  4 """
+
+
+""" Лямбда-выражения """
+
+# square = lambda n: n * n
+# print(square(4))     #  16
+# print(square(5))     #  25
+
+# def kvadrat(n):
+#     return n * n
+# print(kvadrat(4))    #  16
+
+
+# square = lambda n,p: n * p
+# print(square(4))     #  16
+# print(square(5))     #  25
+
+# def kvadrat(n):
+#     return n * n
+# print(kvadrat(4))    #  16
+
+
+""" def select(choice):
+    if choice == 1:
+        return lambda a,b: a + b
+    elif choice == 2:
+        return lambda a,b: a - b
+    else:
+        return lambda a,b: a * b
+    
+oper1 = select(1)
+print(oper1(1,2))    #  3 """
+
+
+
+""" Преобразование типов """
+
+# number1 = 60
+# number2 = 12.5
+# result = number1 + number2
+# print(result)               #  72.5
+# print(type(result))         #  <class 'float'>2
+
+
+# a = '2'
+# b = 2
+# c = int(a) + b
+
+""" # Global variable
+globalvar = "Myname"
+def test():
+# Local variable
+    word = "Word"
+    print("Hello World", word)    # ?
+
+def test2():
+    print("!Hello World")
+
+print(word) """
+
+
+
+""" secret = 123
+
+def test2():
+    # global secret  # переводит в глобальную переменную                       
+    secret = 1234                                                #  ?
+    print("Секретная переменная:", secret)
+test2()
+print(secret)   #  123 """
+
+
+
+"""                                              Вложенность                                             """
+
+""" def outher():
+    n = 5  #  Лексическое окружение
+
+    def inner():
+        nonlocal n
+        n = 25                                                #  изучить
+        print(n)
+
+    inner()
+    # print(n)      #  print(n)
+outher()        #   25 """
+
+
+
+# def outher():           #  Внешняя функция
+#     n = 5               #  Лексическое окружение
+
+#     def inner():        #  Локальная функция
+#         nonlocal n 
+#         n += 1          #  Операция с лесическим окружением
+#         print(n)
+#     return inner
+# """                                                     Замыкание          """
+# plus = outher()
+# # 
+# plus()
+# plus()
+# plus()
+
+
+""" Задача: Напишите функцию createCounter(), которая возвращает другую функцию. Возвращаемая функция при каждом вызове должна увеличивать счетчик и возвращать его текущее значение. """
+
+
+""" def createCounter():
+    n = 0
+
+    def createCounter1():
+        nonlocal n
+        n += 1
+        return n
+    return createCounter1
+createCounter1 = createCounter()
+print(createCounter1())
+print(createCounter1())
+print(createCounter1())
+ """
+
+def createCounter():
+    n = 0
+
+    def createCounter1():
+        nonlocal n
+        n += 1
+        print(n)
+    return createCounter1
+createCounter1 = createCounter()
+createCounter1()
+createCounter1()
+createCounter1()
